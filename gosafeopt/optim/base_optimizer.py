65f726125d40747e7a17a473df9d7835bc9b7edc
Daniel <widmdani@ethz.ch>
2023-08-08 10:03:29 +0200

    Fix cuda error

--- gosafeopt/optim/base_optimizer.py
+++ gosafeopt/optim/base_optimizer.py
@@ -105,7 +105,7 @@ class BaseOptimizer:
             state = OptimizerState(self.c)
             N = self.c["set_size"]
 
-            safeSet = torch.vstack(state.safeSet) if goSafeOptS1Override and state.getSafeSet() is not None else state.getSafeSet()
+            safeSet = torch.vstack(state.safeSet.to(gosafeopt.device)) if goSafeOptS1Override and state.getSafeSet() is not None else state.getSafeSet()
 
             #TODO this doesn't work for different contexts
             if safeSet is None or len(safeSet) == 0:
