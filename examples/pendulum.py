4ffa337b1ff09811ece650663419b09ba68bee33
Daniel <widmdani@ethz.ch>
2023-11-21 17:38:57 +0100

    Switched to pyproject toml. Some cleanup from experiments

--- examples/pendulum.py
+++ examples/pendulum.py
@@ -7,6 +7,7 @@ from gosafeopt.aquisitions.go_safe_opt import GoSafeOptState
 from gosafeopt.aquisitions.max_mean import MaxMean
 from gosafeopt.aquisitions.safe_opt_multistage import SafeOptMultiStageState
 from gosafeopt.aquisitions.safe_ucb import SafeUCB
+from gosafeopt.aquisitions.safe_lcb import SafeLCB
 from gosafeopt.experiments.backup import Backup
 from gosafeopt.experiments.experiment import Experiment
 from gosafeopt.models import create_model
@@ -18,27 +19,27 @@ from pendulum.environments import PendulumGymEnvWithDynamics
 from gosafeopt.tools.data import Data
 from gosafeopt.tools.logger import Logger
 from gosafeopt.trainer import Trainer
-from pendulum.dynamics import U_applied, U_learned, U_ideal
+from pendulum.dynamics import U_learned, U_ideal
 from pendulum.plot import PlotPendulum, PlotPendulumContour
 import torch
-from gosafeopt.tools.data_logger import DataLogger, WandbLogger, load
-from gosafeopt.tools.file import clearFiles, makeGIF
+from gosafeopt.tools.data_logger import load
+from gosafeopt.tools.file import makeGIF
 import matplotlib.pyplot as plt
 from pendulum.environments import PendulumGymEnv
 import json
-from multiprocessing import Process, Pool
 from joblib import Parallel, delayed
-import moviepy
 from moviepy.editor import ImageSequenceClip
 import random
 import gosafeopt
-import time
 from typing import List
 
 app = typer.Typer()
 
 
-def getConfigs(config_path="config.json", config_path_pendulum="config_pendulum.json"):
+def getConfigs(
+    config_path="examples/config.json",
+    config_path_pendulum="examples/config_pendulum.json",
+):
     with open(config_path) as f:
         config = json.load(f)["optimization"]
     with open(config_path_pendulum) as f:
@@ -65,7 +66,7 @@ def plot_gym(
 
     logger = load(f"{data_path}/datalogger.obj")
 
-    config_pendulum['m'] = context1
+    config_pendulum["m"] = context1
     config_pendulum["kp"] *= context1
     config_pendulum["kd"] *= context1
 
@@ -79,11 +80,11 @@ def plot_gym(
     experiment = Experiment(config, env)
     experiment.rollout(k)
 
+
 @app.command()
 def plot_contour(
     i: int = typer.Option(-1, help="Which epoch to simulate"),
-    data_path: str = typer.Option(..., help="data_path")
-
+    data_path: str = typer.Option(..., help="data_path"),
 ):
     config, config_pendulum = getConfigs()
 
@@ -96,7 +97,7 @@ def plot_contour(
 
     plotter = PlotPendulumContour(trajectory_ideal, config, config_pendulum)
     if i == -1:
-        i = len(logger.x_buffer)-1
+        i = len(logger.x_buffer) - 1
     plotter.plotIdx(logger, i)
     plt.show()
 
@@ -105,8 +106,7 @@ def plot_contour(
 def plot(
     i: int = typer.Option(-1, help="Which epoch to simulate"),
     all: bool = typer.Option(False, help="Plot ideal undisturbed trajectory"),
-    data_path: str = typer.Option(..., help="data_path")
-
+    data_path: str = typer.Option(..., help="data_path"),
 ):
     config, config_pendulum = getConfigs()
 
@@ -122,29 +122,28 @@ def plot(
         plotter.plot(logger)
     else:
         if i == -1:
-            i = len(logger.x_buffer)-1
+            i = len(logger.x_buffer) - 1
         plotter.plotIdx(logger, i)
     plt.show()
 
 
 @app.command()
 def train(
-        seed: int = typer.Option(None, help="Seed"),
-        aquisition: str = typer.Option(None, help="Plot ideal undisturbed trajectory"),
-        dry_run: bool = typer.Option(False, help="Plot ideal undisturbed trajectory"),
-        data_dir: str= typer.Option(None, help="data_dir"),
-        contexts: List[float] = typer.Option([1], help="Gate"),
-        wandbdir: str = typer.Option(None, help="wandbdirectory"),
-        load_data_dir: str = typer.Option(None, help="Load data directory"),
-        safe_opt_interior_lb: float = typer.Option(None, help="Interior lb"),
-        safe_opt_marginal_lb: float = typer.Option(None, help="marginal lb"),
-        safe_opt_interior_prob: float = typer.Option(None, help="interior prob"),
-        safe_opt_marginal_prob: float = typer.Option(None, help="marginal prob"),
-        sigma: float = typer.Option(None, help="sigma"),
-        beta: float = typer.Option(None, help="Beta"),
-        config_path: str = typer.Option("config.json", help="Config path"),
+    seed: int = typer.Option(None, help="Seed"),
+    aquisition: str = typer.Option(None, help="Plot ideal undisturbed trajectory"),
+    dry_run: bool = typer.Option(False, help="Plot ideal undisturbed trajectory"),
+    data_dir: str = typer.Option(None, help="data_dir"),
+    contexts: List[float] = typer.Option([1], help="Gate"),
+    wandbdir: str = typer.Option(None, help="wandbdirectory"),
+    load_data_dir: str = typer.Option(None, help="Load data directory"),
+    safe_opt_interior_lb: float = typer.Option(None, help="Interior lb"),
+    safe_opt_marginal_lb: float = typer.Option(None, help="marginal lb"),
+    safe_opt_interior_prob: float = typer.Option(None, help="interior prob"),
+    safe_opt_marginal_prob: float = typer.Option(None, help="marginal prob"),
+    sigma: float = typer.Option(None, help="sigma"),
+    beta: float = typer.Option(None, help="Beta"),
+    config_path: str = typer.Option("config.json", help="Config path"),
 ):
-
     for context in contexts:
         config, config_pendulum = getConfigs(config_path=config_path)
         config.update(config_pendulum)
@@ -152,13 +151,15 @@ def train(
         config["context"] = context
 
         Logger.setVerbosity(4)
-        Logger.info("Using: {} with device {}".format(config["aquisition"], gosafeopt.device))
+        Logger.info(
+            "Using: {} with device {}".format(config["aquisition"], gosafeopt.device)
+        )
 
         if beta is not None:
-            config["beta"] = beta 
+            config["beta"] = beta
 
         if sigma is not None:
-            config["safe_opt_sigma"] = sigma 
+            config["safe_opt_sigma"] = sigma
 
         if safe_opt_interior_lb is not None:
             config["safe_opt_interior_lb"] = safe_opt_interior_lb
@@ -194,7 +195,7 @@ def train(
         data = Data()
         backup = Backup(config, data) if config["aquisition"] == "GoSafeOpt" else None
 
-        config_pendulum['m'] = context
+        config_pendulum["m"] = context
         config_pendulum["kp"] *= context
         config_pendulum["kd"] *= context
 
@@ -208,17 +209,21 @@ def train(
         context = torch.tensor([1.0])
 
         x_safe = torch.tensor(
-            [[-1, 1, context],
-             # [0.1, -0.1, context],
-             # [-0.1, 0.1, context],
-             # [0.1, 0.1, context],
-             # [-0.1, -0.1, context],
-             ])
+            [
+                [-1, 1, context],
+                # [0.1, -0.1, context],
+                # [-0.1, 0.1, context],
+                # [0.1, 0.1, context],
+                # [-0.1, -0.1, context],
+            ]
+        )
 
         envIdeal = PendulumGymEnvWithDynamics(config_pendulum, U_ideal)
         experimentIdeal = Experiment(config, envIdeal)
 
-        _, trajectory_ideal, backup_triggered, info = experimentIdeal.rollout(np.zeros(2))
+        _, trajectory_ideal, backup_triggered, info = experimentIdeal.rollout(
+            np.zeros(2)
+        )
 
         logger = PendulumWandbLogger(config, config_pendulum, trajectory_ideal, context)
 
@@ -228,7 +233,13 @@ def train(
         if config["log_video"]:
             seq = ImageSequenceClip("{}/plot".format(wandb.run.dir), fps=4)
             seq.write_gif("{}/plot/animation.gif".format(wandb.run.dir), fps=4)
-            wandb.log(data={"animation": wandb.Image("{}/plot/animation.gif".format(wandb.run.dir))})
+            wandb.log(
+                data={
+                    "animation": wandb.Image(
+                        "{}/plot/animation.gif".format(wandb.run.dir)
+                    )
+                }
+            )
 
         OptimizerState(config).reset()
         GoSafeOptState(config).reset()
@@ -271,13 +282,11 @@ def ml(
     config_path: str = typer.Option("config.txt", help="Path to config file"),
     data_path: str = typer.Option("data", help="Path data"),
 ):
-
     config, config_pendulum = getConfigs()
     context = torch.tensor([1.0])
 
     config["swarmopt_n_iterations"] = 100
     config["set_size"] = 5000
-
 
     torch.manual_seed(config["seed"])
     np.random.seed(config["seed"])
@@ -285,7 +294,6 @@ def ml(
     data = Data()
     data.load(data_path)
 
-    
     model = create_model(config, data)
     aquisition = SafeUCB(model, config, context, data=data)
     optimizer = get_optimizer(aquisition, config, context)
@@ -298,6 +306,37 @@ def ml(
     Logger.info(f"Best reward for context {context} is: {reward}")
 
 
+@app.command()
+def lower():
+    CONTEXT = {
+        "STAND": [0, torch.tensor([0.75, 1.2, 0.25, 0.5, 0.75])],
+        "WALK": [1, torch.tensor([0.75, 0.8, 0.25, 0.5, 0.75])],
+        "TROT": [2, torch.tensor([0.5, 0.5, 0.5, 0.5, 0])],
+        "CRAWL": [3, torch.tensor([0.75, 1.2, 0.25, 0.5, 0.75])],
+        "FLYING_TROT": [4, torch.tensor([0.4, 0.6, 0.5, 0.5, 0])],
+        "PACE": [5, torch.tensor([0.5, 0.6, 0.5, 0.5, 0])],
+        "PRONK": [6, torch.tensor([0.9, 0.6, 0.0, 0.0, 0.0])],
+        "BOUND": [7, torch.tensor([0.5, 0.4, 0.5, 0.5, 0])],
+        "HOP_TROT": [8, torch.tensor([0.35, 1, 0.5, 0.5, 0])],
+    }
+
+    torch.manual_seed(50)
+    np.random.seed(50)
+
+    data = Data()
+    data.load("/var/home/dw/development/gosafeopt/optim/")
+
+    with open("/var/home/dw/development/gosafeopt/optim/config.json") as f:
+        config = json.load(f)["optimization"]
+
+    model = create_model(config, data)
+
+    context = CONTEXT["FLYING_TROT"][1]
+    aquisition = SafeLCB(model, config, context, data=data)
+    optimizer = get_optimizer(aquisition, config, context)
+    k, acf_val = optimizer.getNextPoint()
+
+    print(k)
 
 
 @app.command()
